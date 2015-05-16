47a1256ec032354f0bc850bd03bdabacd87a710d
Jonathan Etters <ettersj@gmail.com>
2015-05-16 14:42:23 -0500

    QR code capture initiated through button press

--- MessageOnABottle/app/src/main/java/com/example/drew/messageonabottle/ScanCodeActivity.java
+++ MessageOnABottle/app/src/main/java/com/example/drew/messageonabottle/ScanCodeActivity.java
@@ -4,14 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 
-public class ScanCodeActivity extends ActionBarActivity {
+public class ScanCodeActivity extends ActionBarActivity implements View.OnClickListener {
+
+    Button scanCodeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_code);
+
+        scanCodeButton = (Button) findViewById(R.id.scan_code_button);
+        scanCodeButton.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +43,13 @@ public class ScanCodeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.initiateScan();
     }
 }
