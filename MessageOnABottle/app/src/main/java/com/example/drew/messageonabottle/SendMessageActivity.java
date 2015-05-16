690e937b417141d8aaf2e54c9195c766b9ede736
Drew Ryder <drewjryder@gmail.com>
2015-05-16 15:22:20 -0500

    Call REST API to receive message

--- MessageOnABottle/app/src/main/java/com/example/drew/messageonabottle/SendMessageActivity.java
+++ MessageOnABottle/app/src/main/java/com/example/drew/messageonabottle/SendMessageActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-
 public class SendMessageActivity extends ActionBarActivity {
 
     private Toast _toast;
